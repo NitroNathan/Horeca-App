5e069c9a7a46dd8ecb68aa8be9f7ffbc0e5e70c4
NathanH <99060210@mydavinci.nl>
2019-11-01 10:04:05 +0100

    script schrijven

--- HorecaApp.js
+++ HorecaApp.js
@@ -1,5 +1,19 @@
 function bestelling(){
-var bestelling = prompt ("Welke bestelling wilt u toevoegen?")
+	for (a=1; a=30; a++){
+var bestelling = prompt ("Welke bestelling wilt u toevoegen? 'fris' 'bier' of 'wijn'? typ 'rekening' voor de rekening!")
+
+if (bestelling == 'fris'){
+	var fris = prompt ("hoeveel fris wilt u toevoegen aan uw bestelling?")
+}
+
+if (bestelling == 'bier'){
+	var bier = prompt ("hoeveel bier wilt u toevoegen aan uw bestelling?")
+}
+
+if (bestelling == 'wijn'){
+	var wijn = prompt ("hoeveel wijn wilt u toevoegen aan uw bestelling?")
+
+}
 
 
 
@@ -10,11 +24,12 @@ var bestelling = prompt ("Welke bestelling wilt u toevoegen?")
 
 
 
-
-
-
-
+else if (bestelling == 'rekening'){
+	return false;
+}
 
 
 
 }
+}
+bestelling();
